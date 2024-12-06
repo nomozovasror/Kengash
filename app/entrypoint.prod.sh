265b8acb4be2fb9438a14c952a3a0e4613a0a6a6
nomozovasror <nomozovasroe@gmail.com>
2024-12-06 18:53:23 +0500

    changed

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e  # Exit on any error
+
+echo "Checking script permissions..."
+ls -l /entrypoint.prod.sh
 
 echo "PostgreSQL ishga tushishini kuting..."
 while ! nc -z kengash_db 5432; do
@@ -7,18 +9,12 @@ while ! nc -z kengash_db 5432; do
 done
 echo "PostgreSQL tayyor."
 
-echo "Migratsiyalarni bajarish..."
-python manage.py migrate --no-input
+#echo "Migratsiyalarni bajarish..."
+#python manage.py flush --no-input
+#python manage.py migrate
 
 echo "Statik fayllarni yig'ish..."
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 
-# Django serverni boshqa protsessda ishga tushirish
 echo "Django serverini ishga tushirish..."
-gunicorn config.wsgi:application --bind 0.0.0.0:8000 &
-
-# Botni ishga tushirish
-echo "Botni ishga tushirish..."
-python bot.py &
-
-wait
+exec "$@"
