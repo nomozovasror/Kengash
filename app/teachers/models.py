e29044ef1d6f5bacaeeb24221b591ee54316c534
nomozovasror <nomozovasroe@gmail.com>
2025-02-23 17:11:28 +0500

    add datetime field selected employees

--- app/teachers/models.py
+++ app/teachers/models.py
@@ -157,6 +157,7 @@ class SelectedEmployee(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='selected_employee')
     type = models.CharField(max_length=20, choices=TYPE, null=True)
     which_position = models.CharField(max_length=255, null=True)
+    created_at = models.DateField(null=True)
 
     def __str__(self):
         return self.employee.full_name
