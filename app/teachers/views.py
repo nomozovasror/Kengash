292f204dc713c6f02cc9c888cb72e162a44ee1b1
nomozovasror <nomozovasroe@gmail.com>
2025-02-24 15:11:10 +0500

    add reset status fix post

--- app/teachers/views.py
+++ app/teachers/views.py
@@ -503,20 +503,14 @@ def generate_vote_results_docx(request):
 
 @csrf_exempt
 def reset_all_status(request):
-    if request.method == 'POST':
-        try:
-            # Barcha SelectedEmployee obyektlarini yangilash
-            updated_count = SelectedEmployee.objects.update(status=False, voted=False)
-            return JsonResponse({
-                'status': 'success',
-                'message': f'{updated_count} ta obyektning status va voted qiymatlari False ga o\'zgartirildi'
-            })
-        except Exception as e:
-            return JsonResponse({
-                'status': 'error',
-                'message': str(e)
-            }, status=500)
-    return JsonResponse({
-        'status': 'error',
-        'message': 'Faqat POST so\'rovlari qabul qilinadi'
-    }, status=405)
+    try:
+        updated_count = SelectedEmployee.objects.update(status=False, voted=False)
+        return JsonResponse({
+            'status': 'success',
+            'message': f'{updated_count} ta obyektning status va voted qiymatlari False ga o\'zgartirildi'
+        })
+    except Exception as e:
+        return JsonResponse({
+            'status': 'error',
+            'message': str(e)
+        }, status=500)
